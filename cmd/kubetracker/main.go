f72157067dfcefe9134d2420f3067aa3c7d7824a
Vladimir Kuznichenkov <kuzaxak.tech@gmail.com>
2025-08-14 16:34:14 +0300

    Silnce klogger to remove noise from throttler

--- cmd/kubetracker/main.go
+++ cmd/kubetracker/main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"os"
 
+	"github.com/go-logr/logr"
 	"github.com/x-qdo/kubetracker/internal/cli"
+	"k8s.io/klog/v2"
 )
 
 // version is injected via -ldflags "-X main.version=<value>", defaults to "dev".
 var version = "dev"
+
+func init() {
+	klog.SetLogger(logr.Discard())
+}
 
 func main() {
 	// Delegate to internal CLI. It returns a process exit code.
